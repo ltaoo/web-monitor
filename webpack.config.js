a5e6e8898b718466b2b41c63920cc6bf5822b6b1
ltaoo <litaowork@aliyun.com>
2019-05-02 00:43:05 +0800

    chore: 使用 webpack 打包 content 脚本

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+function resolve(p) {
+    return path.resolve(__dirname, p);
+}
+
+module.exports = {
+    entry: './src/scripts/content',
+    output: {
+        path: resolve('./src'),
+        filename: 'content-entry.js',
+    },
+    resolve: {
+        alias: {
+            vue: resolve('./src/scripts/libs/vue'),
+            ELEMENT: resolve('./src/scripts/libs/element-ui'),
+            Mustache: resolve('./src/scripts/libs/mustache.min'),
+            $: resolve('./src/scripts/libs/jquery.min'),
+        },
+    },
+};
