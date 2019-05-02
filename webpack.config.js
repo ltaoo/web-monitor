50487e53b233fbea1514e68a66187e44be5ca979
ltaoo <litaowork@aliyun.com>
2019-05-02 14:40:14 +0800

    chore: rename

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const path = require('path');
 
-function resolve(p) {
-    return path.resolve(__dirname, p);
+function resolve(...args) {
+    return path.resolve(__dirname, ...args);
 }
+
+const SRC = 'webMonitor';
 
 module.exports = {
     entry: {
-        content: './src/scripts/content',
-        options: './src/scripts/options',
-        background: './src/scripts/background',
-        popup: './src/scripts/popup',
+        content: resolve(SRC, 'scripts/content'),
+        options: resolve(SRC, 'scripts/options'),
+        background: resolve(SRC, 'scripts/background'),
+        popup: resolve(SRC, 'scripts/popup'),
     },
     output: {
-        path: resolve('./src'),
+        path: resolve(SRC),
         filename: '[name]-entry.js',
     },
     resolve: {
         alias: {
-            vue: resolve('./src/scripts/libs/vue'),
-            ELEMENT: resolve('./src/scripts/libs/element-ui'),
-            Mustache: resolve('./src/scripts/libs/mustache.min'),
-            jquery: resolve('./src/scripts/libs/jquery.min'),
+            vue: resolve(SRC, 'scripts/libs/vue'),
+            ELEMENT: resolve(SRC, 'scripts/libs/element-ui'),
+            Mustache: resolve(SRC, 'scripts/libs/mustache.min'),
+            jquery: resolve(SRC, 'scripts/libs/jquery.min'),
         },
     },
     mode: 'development',
     watchOptions: {
         ignored: [
-            'options.js',
-            'components',
             'node_modules',
         ],
-        poll: 1000,
+        poll: 4000,
     },
 };
